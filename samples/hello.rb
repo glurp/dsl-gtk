77176b3079a7d8fc93ee1ffb0d756216d570fa55
Regis d'Aubarede <regis.aubarede@gmail.com>
2011-12-08 19:22:52 +0100

    hello world

---
+++ samples/hello.rb
@@ -0,0 +1,47 @@
+require_relative '../lib/ruiby'
+
+
+class RubyApp1 < Ruiby_gtk
+	def component() slot( label("Hello, world!") )  end
+end
+
+class RubyApp2 < Ruiby_gtk
+	def component()  
+		ed =slot( source_editor(width: 300, height: 100) )
+		ed.editor.buffer.text="\ninclude Hello 'world' !"  
+	end
+end
+
+class RubyApp3 < Ruiby_gtk
+	def component() slot( button("Hello, ") { log(ask "world !") } )  end
+end
+
+class RubyApp4 < Ruiby_gtk
+	def component() slot( button("Hello, ") { 
+		w=RubyApp1.new("hx",200,200) 
+		w.window_position(1,1) 
+	  })  
+	end
+end
+
+class RubyApp0 < Ruiby_gtk
+	def component() 
+		lc=File.read(__FILE__).split(/cl[a]ss/)
+		lcode=lc[1..-2].map { |code| "cla"+"ss "+ code }
+		democode= "cla"+"ss "+ lc.last
+		stack do
+			slot( label("\n\n\n\tPlease, Choose one of this hello-world version... \n\n",:font=> "Verdana 12") )
+			lcode.each_with_index { |code,i| slot(button(code) { system("ruby",$0,(i+1).to_s) }) }
+			slot(button("What's that ?") {
+				Editor.new(self,democode)
+			})
+		end
+		window_position(300,300)
+	end
+end
+
+
+
+Gtk.init
+window = eval("RubyApp"+(ARGV[0]||'0')).new("Hw",200,100)
+Gtk.main
