a3e27dea233a582f8a39a5e6651191c0d6d5ac02
Regis d'Aubarede <regis.aubarede@gmail.com>
2012-02-13 21:21:00 +0100

    a

--- gitc.bat
+++ gitc.bat
@@ -4,19 +4,21 @@ IF DEFINED %1=="" (
 	ruby  -e "a=File.read('VERSION').split('.') ; a[-1]=(a.last.to_i+1).to_s; puts r=a.join('.'); File.open('VERSION','w') {|f| f.write(r)}"
 	git commit -a -m "%1 %2 %3 %4 %5 %6 %7 %8 %9"
 	git push
+	goto :eof
 )
 :gem
 ruby -e "Dir.glob('Ruiby*.gem').each {|f| File.delete(f) }"
 call gem build Ruiby.gemspec
 call :test_gem
-call gem push Ruiby.gemspec
+call gem push Ruiby*.gem
 goto :eof
 
 :test_gem
-echo "test_gem..."
+echo
+echo test_gem...
 cd ..
 call gem install Ruiby/Ruiby*.gem
-ruby -e "print File.read('Ruiby/samples/test.rb').gsub(/require_relative.*/,'require 'ruiby')" > RuibyGemTest/test.rb 
+ruby -e "print File.read('Ruiby/samples/test.rb').gsub(/require_relative.*uiby.*/,'require \"ruiby\"')" > RuibyGemTest/test.rb 
 ruby  RuibyGemTest/test.rb
 cd Ruiby 
 goto :eof
