89f7ed1b3a9bd7d2419b43398e9f398badba0f49
Regis d'Aubarede <regis.aubarede@gmail.com>
2012-02-29 20:37:54 +0100

    *  create changelog

--- gitc.bat
+++ gitc.bat
@@ -2,7 +2,8 @@
 IF DEFINED %1=="" (
 	call giti
 	ruby  -e "a=File.read('VERSION').split('.') ; a[-1]=(a.last.to_i+1).to_s; puts r=a.join('.'); File.open('VERSION','w') {|f| f.write(r)}"
-	git commit -a -m "%1 %2 %3 %4 %5 %6 %7 %8 %9"
+	echo "%1 %2 %3 %4 %5 %6 %7 %8 %9" >> CHANGELOG.txt
+	git commit -a -m "*  %1 %2 %3 %4 %5 %6 %7 %8 %9"
 	git push
 	echo
 	echo call gitc.bat without args for make/post rubygem
@@ -11,6 +12,7 @@ IF DEFINED %1=="" (
 :gem
 rem ==== no args, pgenerate gem and push it to rubygems.org
 
+ruby  -e "a=File.read('VERSION').split('.').pop ; a[-1]=(a.last.to_i+1).to_s; puts r=(a+%{0}).join('.'); File.open('VERSION','w') {|f| f.write(r)}"
 ruby -e "Dir.glob('Ruiby*.gem').each {|f| File.delete(f) }"
 call gem build Ruiby.gemspec
 call :test_gem
