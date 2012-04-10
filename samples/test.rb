6e783bcf4fbb8a34824dd650a9003d0b16bc896c
Regis d'Aubarede <regis.aubarede@gmail.com>
2012-04-10 20:31:02 +0200

    explode component() to multiples litle function

--- samples/test.rb
+++ samples/test.rb
@@ -2,7 +2,7 @@
 # encoding: utf-8
 $time_start=Time.now.to_f*1000
 def mlog(text)
- puts "%8f | %s" % [(Time.now.to_f*1000-$time_start),text.to_s]
+ puts "%8f | %s" % [(Time.now.to_f*1000-$time_start).to_i,text.to_s]
 end
 mlog 'before require gtk2'
 require 'gtk2'
@@ -31,63 +31,86 @@ def component()
 	   ))
     sloti(label( <<-EEND ,:font=>"Arial 12"))
      This window is test & demo of Ruiby capacity,
-	   ~ 170 Lines of code,
-     (Ruiby version is #{Ruiby::VERSION})
+	   ~ 180 Lines of code,
+     Ruiby version is #{Ruiby::VERSION}, Gtk version is #{Gtk::VERSION.join(".")}
 	EEND
-	
-    
     separator
     flow {
       @left=stack {
-        frame("") { table(2,10,{set_column_spacings: 3}) do
-          row { cell_right(label  "mode de fontionnement"); cell(button("set") { alert("?") }) }
-          row { cell_right label  "vitesse"               ; cell(entry("aa"))  }
-          row { cell_right label  "size"                  ; cell ientry(11,{:min=>0,:max=>100,:by=>1})  }
-          row { cell_right label  "feeling"               ; cell islider(10,{:min=>0,:max=>100,:by=>1})  }
-          row { cell_right label  "speedy"                ; cell(toggle_button("on","off",false) {|w| w.label=w.active?() ? "Off": "On" })  }
-          row { cell       label  "acceleration type"     ; cell hradio_buttons(%w{aa bb cc},1)  }
-          row { cell      label  "mode on"               ; cell check_button("",false)  }
-          row { cell      label  "mode off"              ; cell check_button("",true)  }
-          row { cell_left label  "Variable"              ; cell combo({"aaa"=>1,"bbb"=>2,"ccc"=>3},1) }
-          row { p 4;cell_left label  "Couleur"               ; cell color_choice()  }
-        end }
+		test_table
+		test_canvas
+     }
+	separator
+      notebook do
+        page("","#home") { label("A Notebook Page with icon as button-title",{font: "Arial 18"}) }
+        page("List & grid") { test_list_grid }
+        page("Property Edit.") { test_properties(0) }
+        page("Big PropEditor") { test_properties(1) }
+        page("Source Editor") {
+		  @editor=source_editor(:width=>200,:height=>300,:lang=> "ruby", :font=> "Courier new 8",:on_change=> proc { edit_change }).editor
+		  @editor.buffer.text='def comp'+'onent'+File.read(__FILE__).split(/comp[o]nent/)[1]
+        }
+		page("Menu") { test_menu }
+        page("Accordion") { test_accordion }
+		page("Pan & Scrolled") { test_pan_scroll}
+	  end # end notebook
+    } # end flow
+    sloti(button("Test Specials Actions...") { p @bref ; do_special_actions() })
+    sloti( button("Exit") { exit! })
+	mlog 'after Component'
+  end
+end
+
+	def test_table	
+		frame("") { table(2,10,{set_column_spacings: 3}) do
+			  row { cell_right(label  "mode de fontionnement"); cell(button("set") { alert("?") }) }
+			  row { cell_right label  "vitesse"               ; cell(entry("aa"))  }
+			  row { cell_right label  "size"                  ; cell ientry(11,{:min=>0,:max=>100,:by=>1})  }
+			  row { cell_right label  "feeling"               ; cell islider(10,{:min=>0,:max=>100,:by=>1})  }
+			  row { cell_right label  "speedy"                ; cell(toggle_button("on","off",false) {|w| w.label=w.active?() ? "Off": "On" })  }
+			  row { cell       label  "acceleration type"     ; cell hradio_buttons(%w{aa bb cc},1)  }
+			  row { cell      label  "mode on"               ; cell check_button("",false)  }
+			  row { cell      label  "mode off"              ; cell check_button("",true)  }
+			  row { cell_left label  "Variable"              ; cell combo({"aaa"=>1,"bbb"=>2,"ccc"=>3},1) }
+			  row { p 4;cell_left label  "Couleur"               ; cell color_choice()  }
+			end 
+		}
         frame("Buttons in frame") {
           flow { sloti(button("packed with sloti()") {alert("button packed with sloti()")}) 
 		         @bref=sloti(button("bb")) ;  button("packed with slot()") ; 
 		  }
         }
-        flow do
-          stack {
-            button("Couleur") {
-              #alert("alert !") ; error("error !") ; ask("ask !") ;trace("trace !") ;
-              @color=ask_color()
-            }
-            sloti(label('Epaisseur'))
-            @epaisseur=sloti(islider(1,{:min=>1,:max=>30,:by=>1}))
-          }
-          @ldraw=[] ; @color=  ::Gdk::Color.parse("#33EEFF");
-          canvas(100,100,{ 
-            :expose     => proc { |w,cr|  
-              @ldraw.each do |line|
-                next if line.size<3
-                color,ep,pt0,*poly=*line
-                cr.set_line_width(ep)
-                cr.set_source_rgba(color.red/65000.0, color.green/65000.0, color.blue/65000.0, 1)
-                cr.move_to(*pt0)
-                poly.each {|px|    cr.line_to(*px) } 
-                cr.stroke  
-            end
-            },          
-            :mouse_down => proc { |w,e|   no= [e.x,e.y] ;  @ldraw << [@color,@epaisseur.value,no] ;  no    },
-            :mouse_move => proc { |w,e,o| no= [e.x,e.y] ; (@ldraw.last << no) if no[0]!=o[0] || no[1]!=o[1] ; no },
-            :mouse_up   => proc { |w,e,o| no= [e.x,e.y] ; (@ldraw.last << no) ; no}
-            })
-        end 
-      }
-      separator
-      notebook do
-        page("","#home") { label("A Notebook Page with icon as button-title",{font: "Arial 18"}) }
-        page("List & grid") {
+	end
+	def test_canvas()
+		 flow do
+			  stack {
+				button("Couleur") {
+				  #alert("alert !") ; error("error !") ; ask("ask !") ;trace("trace !") ;
+				  @color=ask_color()
+				}
+				sloti(label('Epaisseur'))
+				@epaisseur=sloti(islider(1,{:min=>1,:max=>30,:by=>1}))
+			  }
+			  @ldraw=[] ; @color=  ::Gdk::Color.parse("#33EEFF");
+			  canvas(100,100,{ 
+				:expose     => proc { |w,cr|  
+				  @ldraw.each do |line|
+					next if line.size<3
+					color,ep,pt0,*poly=*line
+					cr.set_line_width(ep)
+					cr.set_source_rgba(color.red/65000.0, color.green/65000.0, color.blue/65000.0, 1)
+					cr.move_to(*pt0)
+					poly.each {|px|    cr.line_to(*px) } 
+					cr.stroke  
+				end
+				},          
+				:mouse_down => proc { |w,e|   no= [e.x,e.y] ;  @ldraw << [@color,@epaisseur.value,no] ;  no    },
+				:mouse_move => proc { |w,e,o| no= [e.x,e.y] ; (@ldraw.last << no) if no[0]!=o[0] || no[1]!=o[1] ; no },
+				:mouse_up   => proc { |w,e,o| no= [e.x,e.y] ; (@ldraw.last << no) ; no}
+				})
+			end 
+	 end
+	 def test_list_grid()
 			flow {
 				frame("List") {
 					stack {
@@ -110,25 +133,23 @@ def component()
 			}
 			10.times { |i| @list.add_item("Hello #{i}") }
 			@grid.set_data((1..30).map { |n| ["e#{n}",n,1.0*n]})
-        }
-        page("Property Edit.") {
-          flowi {
-			sloti(button("#harddisk") { alert("image button!")})
-			tt={int: 1,float: 1.0, array: [1,2,3], hash: {a:1, b:2}}
-			propertys("props editable",tt,{edit: true}) { |a| log(a.inspect);log(tt.inspect) }
-			propertys("props show",tt)
-		  }
-		  calendar()
-	    }
-        page("Big PropEditor") {
+	end
+	def test_properties(no)
+		case no 
+	     when 0
+			flowi {
+				sloti(button("#harddisk") { alert("image button!")})
+				tt={int: 1,float: 1.0, array: [1,2,3], hash: {a:1, b:2}}
+				properties("props editable",tt,{edit: true}) { |a| log(a.inspect);log(tt.inspect) }
+				properties("props show",tt)
+		    }
+			calendar()
+		when 1
 			h={};70.times { |i| h[i]= "aaa#{i+100}" }
-			propertys("very big propertys editable",h,{edit: true,scroll: [100,400]}) { |a| log(a.inspect);log(h.inspect) }
-        }
-        page("Source Editor") {
-		  @editor=source_editor(:width=>200,:height=>300,:lang=> "ruby", :font=> "Courier new 8",:on_change=> proc { edit_change }).editor
-		  @editor.buffer.text='def comp'+'onent'+File.read(__FILE__).split(/comp[o]nent/)[1]
-        }
-		page("Menu") {
+			properties("very big propertys editable",h,{edit: true,scroll: [100,400]}) { |a| log(a.inspect);log(h.inspect) }
+		end
+	end
+	def test_menu
 			stack {
 				menu_bar {
 					menu("File Example") {
@@ -146,8 +167,8 @@ def component()
 				} 
 				@f=stacki { }
 			}
-		}
-        page("Accordion") {
+	end
+	def test_accordion()
 			flow {
 				accordion do
 					("A".."G").each do |cc| 
@@ -160,8 +181,8 @@ def component()
 				end
 				label "x"
 			}
-		}
-		page("Pan & Scrolled") do
+	end
+	def test_pan_scroll()
 			stack {
 				sloti(label("Test scrolled zone"))
 				separator
@@ -177,14 +198,10 @@ def component()
 					}
 				  }] end
 			  }
-	    end      
-	  end # end notebook
-    } # end flow
-    sloti(button("Test Specials Actions...") { p @bref ; do_special_actions() })
-    sloti( button("Exit") { exit! })
-	mlog 'after Component'
-  end
-end # endcomponent
+	end
+	
+# endcomponent
+ 
   def edit_change()
 	alert("please, do not change my code..")
   end
