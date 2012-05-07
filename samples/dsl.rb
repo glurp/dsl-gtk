5fa0d506282d0fff2f0c34014764d45ad3a06a5a
Regis d'Aubarede <regis.aubarede@gmail.com>
2012-05-07 20:29:14 +0200

    creation

---
+++ samples/dsl.rb
@@ -0,0 +1,22 @@
+# show all raster file which names are in  ARGV
+# First example of a Ruiby.app
+require_relative '../lib/ruiby'
+
+if ARGV.size==0
+	Message.alert("Usage\n>ruby   images.rb    raster.png   raster.gif ....")
+	exit
+end
+
+Ruiby.app(:width=> 800, :height=>800) do
+	vbox_scrolled(800,800) do 
+		table(3,ARGV.size) do
+			ARGV.each { |fn| 
+				row { 
+					cell( w=label( "#"+fn ) )
+					cell(flow(false) { properties(fn,get_config(w.pixbuf))  if w.pixbuf })
+					cell(flow(false) { properties(fn,get_config(w),{:scroll => [300,100]})})
+				}
+			}
+		end
+	end
+end
