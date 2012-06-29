ded8d2a8974822c9900b3ba8367d3ad4a1b587d0
Regis d'Aubarede <regis.aubarede@gmail.com>
2012-06-29 22:22:37 +0200

    slection, save (marshal), export svg

--- samples/draw.rb
+++ samples/draw.rb
@@ -23,6 +23,47 @@ end
 ############################## define basic vector widget (not editable)
 #                              end each vector type
 module Vector
+	class Bbox
+		def initialize(*l)
+			@x0,@y0,@x1,@y1=999999,999999,-999999,-999999
+			if l.size==2 && Numeric===l[0]
+				@x0=@x1=l[0]
+				@y0=@y1=l[1]
+			elsif l.size==4 && Numeric===l[0]
+				add_point(l[0],l[1])
+				add_point(l[2],l[3])
+			end
+		end 
+		def values() [@x0,@y0,@x1,@y1] end
+		def add_lpoints(l) l.each { |(x,y)| add_point(x,y) } ; self end
+		def <<(a,b=nil,c=nil,d=nil)
+			if b==nil
+			  Bbox === a ? add_bbox(a) : Array===a ? add_point(*a) : raise("error param bbox << ...")
+			elsif c==nil
+			  add_point(a,b)
+			else
+			  add_point(a,b)
+			  add_point(c,d)
+			end
+			self
+		end
+		def add_point(x,y)
+			@x0=x if x<@x0
+			@y0=x if y<@y0
+			@x1=x if x>@x1
+			@y1=y if y>@y1
+			self
+		end
+		def add_bbox(box)
+			a,b,a1,b1=*box.values
+			@x0=a  if a<@x0
+			@y0=b  if b<@y0
+			@x1=a1 if a1>@x1
+			@y1=b1 if b1>@y1
+			self
+		end
+	end
+	
 	class VCanvas
 		def initialize(win,w,h,options)
 			@layers=[[],[]]
@@ -34,7 +75,43 @@ module Vector
 				:mouse_up   => proc { |w,e,o| self.send(@mode.to_s+"_mouse_up",e)  }
 			})
 		end
+		
+		def prt(*txt) puts "%-80s | %s" % [txt.map { |o| String===o ? o : o.inspect}.join(", ")[0..79],caller[0].to_s.split(/\//)[-1]] end
+		def prt2(*txt) puts "%-80s | %s" % [txt.map { |o| String===o ? o : o.inspect}.join(", ")[0..79],caller[1].to_s.split(/\//)[-1]] end
+		def to_svg(out)
+			str=<<EEND
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+<svg
+   xmlns:svg="http://www.w3.org/2000/svg"
+       xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   x-width="%W%"
+   x-height="%H%"
+   viewport="0 0 %W% %H%"
+   version="1.1"
+   sodipodi:docname="%NAME%">
+EEND
+			x0,y0,x1,y1=bbox().values
+			out << str.gsub(/%[^%]+%/,{ 
+				"%W%" => x1.to_s,
+				"%H%" => y1.to_s,
+				"%NAME%" => "draw.svg"
+			})
+			@layers[1].each{ |v| v.to_svg(out)  }
+			out << "\n</svg>"
+		end
+		def save_to_rdr(name)
+			File.open(name,"wb") { |f| Marshal.dump(@layers[1],f) } 
+		end
+		def load_from_rdr(name)
+			File.open(name,"r") { |f| @layers[1]=Marshal.load(f) }	
+		end
+		def bbox() 
+			@layers[1].inject(Bbox.new) {  |bb,v| v.append_to_bbox(bb) } 
+		end
 		def widget() @cv end
+
 		def clear
 			@layers=[[],[]]
 			@mode=:nil
@@ -57,18 +134,24 @@ module Vector
 			end
 			@cv.redraw()
 		end
-		def find_cover_bbox(x0,y0,x1,y1)
-			@layers[1].select { |v| v.cover_bbox?(x0,y0,x1,y1) }
+		
+		#------------------ Query
+		
+		def find_cover_bbox(x0,y0,x1,y1,nolayer=1)
+			@layers[nolayer].select { |v| v.cover_bbox?(x0,y0,x1,y1) }
 		end
-		def find_into_bbox(x0,y0,x1,y1)
-			@layers[1].select { |v| v.into_bbox?(x0,y0,x1,y1) }
+		def find_into_bbox(x0,y0,x1,y1,nolayer=1)
+			@layers[nolayer].select { |v| v.into_bbox?(x0,y0,x1,y1) }
 		end
-		def find_nerest(x0,y0)
+		def find_nearest(x0,y0,nolayer=1)
 			return nil if @layers[1].size==0
-			l=@layers[1].map { |v| [v,v.distance(x0,y0)] }.sort { |a,b| 
+			l=@layers[nolayer].map { |v| [v,v.distance(x0,y0)] }.sort { |a,b| 
 				a[1] <=> b[1] 
 			}
 			l[0][0]
+		end
+		def find_hoover(x0,y0,nolayer=1)
+			@layers[nolayer].select { |v| v.hoover_point?(x0,y0) }[0]
 		end
 		def nil_mouse_down(e) end
 		def nil_mouse_move(e) redraw(e) end
@@ -77,9 +160,31 @@ module Vector
 	end
 	class VElem
 		def initialize() @lpoints=[];@style={} end
+		def bbox()
+			@lpoints.inject(Bbox.new){ |bb,p| bb << [p[0],p[1]] }
+		end
+		def append_to_bbox(box)
+			@lpoints.each { |p| box << [p[0],p[1]] }
+			box
+		end
+		def vclone()
+			n=self.clone
+			n.lpoints=lpoints.clone
+			n.set_style(@style.clone)
+			n
+		end
+		def to_svg(out)
+			out << "  <#{self.svg_tag()} dr='#{self.class}' d='#{self.svg_path()}' style='#{self.svg_style}'/>\n"
+		end
+		def svg_tag() "path" end
+		def svg_path() "M"+lpoints.map {|p| "#{p[0]},#{p[1]}" }.join(" L") end
+		def svg_style() "fill:##{svgc(:fill_color)};fill-opacity:#{1};stroke:##{svgc(:stroke_color)};stroke-width:#{@style[:stroke_width]||"0"};" end
+		def svgc(name)  @style[name] ? "%02X%02X%02X" % @style[name].map { |v| (v*255).round } : "000000" end
+	
 		def lpoints() @lpoints end
 		def lpoints=(l) @lpoints=l end
 		def set_style(s) @style=s.clone	end
+		def get_style(s) @style.clone	end
 		def draw(w,ctx)
 			return if @lpoints.size==0
 			define_style_before(ctx)
@@ -99,10 +204,16 @@ module Vector
 		def rotate(x0,y0,r) 
 			a=Math::PI*(r/180.0)
 			@lpoints.map! { |(x,y)| [x0+(x-x0)*Math.cos(a)+(y-y0)*Math.sin(a),y0-(x-x0)*Math.sin(a)+(y-y0)*Math.cos(a)] }
+			self
+		end
+		def deplace(dx,dy)
+			@lpoints.map! { |(x,y)| [x+dx,y+dy] }
+			self
 		end
 		def scale(x0,y0,rx,ry=nil) 
 			ry=rx unless ry
 			@lpoints.map! { |(x,y)| [x0+(x-x0)*rx,y0+(y-y0)*ry] }
+			self
 		end
 		def distance(x0,y0)
 			return(2_999_999_999) if @lpoints.size==0
@@ -118,12 +229,16 @@ module Vector
 		def pt_in_bbox?(x,y, x0,y0,x1,y1)
 			(x>=x0 && x<= x1 && y>=y0 && y<=y1)
 		end
+		def hoover_point?(x,y)
+			distance(x,y)<10
+		end
 	end
 	
 	class VElem_surface < VElem
 		def define_style_before(ctx)
 			ctx.set_line_width(@style[:stroke_width]) if @style[:stroke_width]
 		end
+		def svg_path() super()+" Z" end
 		def render() raise('abstract') end
 		def define_style_after(ctx)
 			if @style[:stroke_width]&&@style[:stroke_width]>0
@@ -176,6 +291,15 @@ module Vector
 		end
 	end
 	class SelMark < VElem
+		def initialize(sel_vector=nil,index=nil)
+			@sel_vector,@index=sel_vector,index
+		end
+		def deplace(dx,dy)
+			return(self) if @lpoints.size!=1
+			super
+			@sel_vector.lpoints[@index]=@lpoints[0] if @sel_vector && @index
+			self
+		end
 		def define_style_before(ctx)
 			ctx.set_line_width(1) 
 			ctx.set_source_rgba(0.3,0.3,1,0.8)
@@ -202,6 +326,9 @@ module Vector
 	class Text		< VElem
 		def set_text(text)
 			@text=text
+		end
+		def to_svg(out)
+			out << "  <text dr='#{self.class}' x='#{@lpoints[0][0]}' y='#{@lpoints[0][1]}' style='#{self.svg_style}'>#{@text}</text>\n"
 		end
 		def render(ctx)
 			return if @lpoints.size!=1
@@ -235,21 +362,37 @@ module Vector
 		def initialize(win,w,h,options)
 			super
 			@current=nil
+			@select={}
+			@corbeill=[]
 			define_style({})
+			#------------------------ create one vector for easy test
+			@current=Polyline.new
+			@current.lpoints=[[100,200],[100,10],[200,300],[400,300],[200,200]]
+			define_style({})
+			complete_edition()
+			#------------------------
+			mode_selection()
 		end
 		def clear
 			@current=nil
 			super
 		end
 		def define_style(options)
-			@cstyle={
-				:stroke_width=>options[:width] || 1,
-				:stroke_color=>conv_color(options[:fg] || [0,0,0,0]),
-				:fill_color=>conv_color(options[:bg] || [1,1,1]) ,
-				:font=>options[:font]|| "Sans",
-				:font_size=>options[:font_size]|| 10
-			}
-			@current.set_style(@cstyle) if @current
+			if @current 
+				@current.set_style(@cstyle) 
+				redraw
+			end
+			if @select.size>0 && @select[:list] && @select[:list].size>0
+				@select[:list].each { |v| v.set_style(options) }
+			else
+				@cstyle={
+					:stroke_width=>options[:width] || 1,
+					:stroke_color=>conv_color(options[:fg] || [0,0,0,0]),
+					:fill_color=>conv_color(options[:bg] || [1,1,1]) ,
+					:font=>options[:font]|| "Sans",
+					:font_size=>options[:font_size]|| 10
+				}
+			end
 		end
 		def conv_color(color)
 			return color if Array === color
@@ -260,7 +403,7 @@ module Vector
 			@current.draw(w,ctx) if @current
 		end
 		def complete_edition()
-			if @current
+			if @current && @current.lpoints.size>0
 				@layers[1] << @current  
 				@current=@current.clone_empty
 			end
@@ -380,13 +523,25 @@ module Vector
 		################################################################		
 		def mode_selection()
 			complete_edition()
+			@current=nil
 			@select={}
 			@layers[0]=[]
 			@lasso=nil
 			@mode=:select
+			redraw
 		end
-		def make_mark(x,y)
-			m=SelMark.new
+		def selection_vector(lv)
+			@layers[0]=[]
+			@lasso=nil
+			@select[:list]= []
+			lv.each { |v| 
+				v.lpoints.each_with_index { |(x,y),i| make_mark(v,i,x,y) } 
+				@select[:list] << v
+			}
+			mode=:select
+		end
+		def make_mark(v,i,x,y)
+			m=SelMark.new(v,i)
 			m.lpoints=[[x,y]]
 			@layers[0]<<m
 			m
@@ -398,15 +553,51 @@ module Vector
 			@lasso=r
 			r
 		end
-		def selection_vector(lv)
-			@layers[0]=[]
-			@lasso=nil
-			lv.each { |v| v.lpoints.each { |(x,y)| make_mark(x,y) } }
+		def cut()
+			return unless @select[:list]
+			@corbeill=[]
+			l=@select[:list]
+			mode_selection()
+			l.each { |sel| 
+				a,b=@layers[1].partition { |v| v!=sel }
+				@corbeill += b
+				@layers[1]=a
+			}			
+			prt "corbeill=",@corbeill 
+			redraw
+		end
+		def copy()
+			return unless @select[:list]  
+			@corbeill=[]
+			@select[:list].each { |sel| 
+				@corbeill += @layers[1].select { |v| v==sel }
+			}
+			mode_selection()
+			prt "corbeill=",@corbeill 
+		end
+		def past()
+			return if @corbeill.size==0
+			prt " << ",@corbeill 
+			@layers[1] += @corbeill.map { |v| v.vclone.deplace(3,3) }
+			redraw
+		end
+		def copystyle() 
+			copy()
+			@cstyle=@select[:list][0].get_style() if @select[:list] && @select[:list].size>0
+		end
+		def edit() 
+			str=to_svg("")
+			$app.edit(str)
 		end
 		
 		################################## Mouse Interaction for Selection
 		def select_mouse_down(e) 
-			@select[:pt0]=[e.x,e.y]
+			if r=find_hoover(e.x,e.y,0)
+				@select[:vector]=[r,r.lpoints[0].clone]
+				@mode=:mselect
+			else
+				@select[:pt0]=[e.x,e.y]
+			end
 		end
 		def select_mouse_move(e) 
 			@select[:pt1]=[e.x,e.y]
@@ -424,13 +615,23 @@ module Vector
 				l=find_cover_bbox(*@lasso.lpoints.flatten) if l.size==0
 				l.size>0 ? selection_vector(l) : mode_selection()
 			else
-				v=find_nerest(*@select[:pt0])				
+				v=find_nearest(*@select[:pt0])				
 				v ? selection_vector([v]) : mode_selection()
 			end
 			redraw(e) 
 		end
-	end
-end
+		
+		def mselect_mouse_move(e) 
+			x0,y0=*@select[:vector][1]
+			@select[:vector][0].deplace(e.x-x0,e.y-y0)
+			@select[:vector][1]=[e.x,e.y]
+			redraw(e) 
+		end
+		def mselect_mouse_up(e)   
+			mode_selection()
+		end
+	end  # fin class
+end  # fin module
 
 ################################### Drawer application #############################
 
@@ -440,15 +641,29 @@ class Application < Ruiby_gtk
 		@stroke_width=1
 		@fgcolor=Gdk::Color.new(0,0,0)
 		@bgcolor=Gdk::Color.new(1,1,1)
+		@filename=nil
 		@t=nil
 		super("Vector Draw",800,800)  
 	end
+
 	def component()  		
 		stack do
 			################## Menu
 			menu_bar do
-				menu("File") {menu_button("Open") { } ; menu_button("Close") { } ;menu_button("New") { @win.clear} }
-				menu("Edit") {menu_button("Cut") { } ; menu_button("Copy") { } ;menu_button("Past") { } ;menu_button("Copy style") { } }
+				menu("File") {
+					menu_button("Open") { fopen() }
+					menu_button("Save") { fsave() }
+					menu_button("Save as...") { fsave_as() }
+					menu_button("Rename as...") { frename() } 
+					menu_separator
+					menu_button("Export...") { fexport() }
+					menu_button("New") {  fclear()  }
+					menu_separator
+					menu_button("Exit") {  ruiby_exit }
+				}
+				menu("Edit") {
+					menu_button("Cut") { cut() } ; menu_button("Copy") { copy() } ;menu_button("Past") { past() } ;menu_button("Copy style") { copystyle()} 
+				}
 			end
 			############### Central zone
 			flow do
@@ -456,12 +671,12 @@ class Application < Ruiby_gtk
 				stack { stacki do
 				  button("#apply"){ @win.end_create_current()}
 				  separator
-				  button("#media/draw.png[0,0]x32") { @win.mode_create_polyline() ; @labl.text="Create Polyline"}
-				  button("#media/draw.png[1,0]x32") { @win.mode_create_polygone() ; @labl.text="Create Polygone"}
-				  button("#media/draw.png[2,0]x32") { @win.mode_create_rect()     ; @labl.text="Create rectangle"}
-				  button("#media/draw.png[3,0]x32") { @win.mode_create_oval() 	  ; @labl.text="Create oval"}
+				  button("#media/draw.png[0,0]x22") { @win.mode_create_polyline() ; @labl.text="Create Polyline"}
+				  button("#media/draw.png[1,0]x22") { @win.mode_create_polygone() ; @labl.text="Create Polygone"}
+				  button("#media/draw.png[2,0]x22") { @win.mode_create_rect()     ; @labl.text="Create rectangle"}
+				  button("#media/draw.png[3,0]x22") { @win.mode_create_oval() 	  ; @labl.text="Create oval"}
 				  button("#italic")					{ @win.mode_create_text() 	  ; @labl.text="Create text"}
-				  button("#media/draw.png[4,0]x32") { @win.mode_create_image() 	  ; @labl.text="Create image"}
+				  button("#media/draw.png[4,0]x22") { @win.mode_create_image() 	  ; @labl.text="Create image"}
 				end}
 				#------------------ Canvas
 
@@ -470,7 +685,7 @@ class Application < Ruiby_gtk
 				#------------------ Right menu
 				stack {space;stacki do
 					button("Sel.") { @win.mode_selection() }
-					button("#cut");button("#copy");button("#paste");button("#delete");button("#edit")
+					button("#cut") { @win.cut() };button("#copy") { @win.copy() };button("#paste") { @win.past() }; space ; button("#edit") { @win.edit() }
 				end}
 			end
 			
@@ -478,19 +693,27 @@ class Application < Ruiby_gtk
 			flow do
 			  @labl=label("Left label...")
 			  flow {
+				button("#media/draw.png[0,1]x22") { @win.sel_top()      }
+				button("#media/draw.png[1,1]x22") { @win.sel_bottom()   }
+				button("#media/draw.png[2,1]x22") { @win.sel_group()    }
+				button("#media/draw.png[3,1]x22") { @win.sel_ungroup()  }
+				button("#media/draw.png[4,1]x22") { @win.sel_align(0,0) }
+				button("#media/draw.png[5,1]x22") { @win.sel_align(0,1) }
+				button("#media/draw.png[6,1]x22") { @win.sel_align(1,1) }
+				button("#media/draw.png[7,1]x22") { @win.sel_align(1,1) }
 				color_choice("bg") { |c| @bgcolor=c ; define_style}
 				color_choice("fg") { |c| @fgcolor=c ; define_style}
 				label("Width :")
-				islider(1,{:min=>1,:max=>30,:by=>1}) { |v| 
+				ientry(1,{:min=>1,:max=>100,:by=>1}) { |v| 
 					@stroke_width=v.to_i
 					define_style
 				}
 				label("Rotate :")
-				islider(0,{:min=>0,:max=>180,:by=>1}) { |v| 
+				ientry(0,{:min=>0,:max=>180,:by=>1}) { |v| 
 					@win.rotate(400,400,v.to_i)
 				}
 				label("Scale :")
-				islider(0,{:min=>-10,:max=>10,:by=>1}) { |v| 
+				ientry(0,{:min=>-10,:max=>10,:by=>1}) { |v| 
 					@win.scale(400,400,(v.to_i+10)/5.0,(v.to_i+10)/5.0)
 				}
 				label("Animation :")
@@ -498,7 +721,15 @@ class Application < Ruiby_gtk
 			  }
 			  @labr=label("rigtht label...")
 			end
-			button("reload") { load(__FILE__,0) rescue p $! }
+			button("reload") { 
+				puts "----------------------------------"
+				begin
+					load(__FILE__) 
+				rescue Exception => e
+					error($!) 
+				end
+				puts "----------------------------------"
+			}
 		end
 		$app=self
 		define_style
@@ -509,9 +740,49 @@ class Application < Ruiby_gtk
 		}
 	end
 	def define_style
-		@win.define_style({:fg=>@fgcolor,:bg=>@bgcolor,:width=>@stroke_width})
+		@win.define_style({:fg=>@fgcolor,:bg=>@bgcolor,:width=>@stroke_width,:font=>"Arial",:font_size=>33})
 	end
 	def lstatus(text) @labl.text=text.to_s end
 	def rstatus(text) @labr.text=text.to_s end
+	
+	def fopen() 
+		fclear()
+		fn=ask_file_to_read(".","*.rdr")
+		if fn
+			(@win.load_from_rdr(fn);@filename=fn)  rescue errors($!)
+		end
+	end
+	def fsave() 
+		@filename=ask_file_to_write(".","*.rdr") unless  @filename
+		if @filename
+			(@win.save_to_rdr(@filename);alert("#{@filename} saved !")) rescue error($!)
+		end
+	end
+	def fsave_as() 
+		fn=ask_file_to_write(".","*.rdr")
+		if fn
+			@filename=fn
+			fsave()
+		end
+	end
+	def fexport() 
+		fn=ask_file_to_write(".","*.svg")   
+		if fn
+			(File.open(fn,"w") { |f| @win.to_svg(f) };alert("#{fn} saved !")) rescue error($!)
+		end
+	end
+	def frename() 
+		return unless @filename
+		fn=ask_file_to_write(".","*.svg")
+		if fn && File.exists?(@filename)
+			@filename,fn=fn,@filename
+			fsave()
+			File.delete(fn) if File.exists?(fn) && File.exists?(@filename)
+		end
+	end
+	
+	def fclear() @win.clear ; @filename=nil end
+
 end
+
 Ruiby.start { Application.new }
