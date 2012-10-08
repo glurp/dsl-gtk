bdd621afd1ea6828d8538331b0a8f8305171a31e
Regis d'Aubarede <regis.aubarede@gmail.com>
2012-10-09 00:38:51 +0200

    complete

--- spec/test_basics_widget.rb
+++ spec/test_basics_widget.rb
@@ -3,14 +3,9 @@ require_relative 'rspec_helper.rb'
 describe Ruiby do
  before(:each) do
 	@win= make_window
-	Ruiby.update
  end
  after(:each) do
-	Ruiby.update
-	sleep(0.05)
-	Ruiby.update
-	@win.destroy 
-	Ruiby.update
+	destroy_window(@win)
  end
  it "create a button" do
 		s=nil;@win.create { s=stack {  } }
@@ -25,6 +20,7 @@ describe Ruiby do
  it "create a label with icon" do
 		s=nil;@win.create { s=stack {  } }
 		@win.append_to(s) { @win.label("#open")  }
+		@win.sleeping(100,"Verify slot/sloty")
 		s.children.size.should eq(1)
  end
  it "create a labeli and buttoni, checking min/auto size" do
@@ -44,7 +40,69 @@ describe Ruiby do
 				buttoni("#open")		
 			}
 		}
-		@win.sleeping(300,"Verify slot/sloty")
+		@win.sleeping(100,"Verify slot/sloty")
 		s.children.size.should eq(4)
  end
+ it "create a label with file image" do
+		w=[];
+		@win.create { stack {  
+			label("file reaster is :")
+			w << label("##{$here}/draw.png")  
+			label("sub part of the raster file :")
+			w << label("##{$here}/draw.png[0,0]x22")  
+			w << label("##{$here}/draw.png[1,0]x22")  
+			w << label("##{$here}/draw.png[2,0]x22")  
+			w << label("##{$here}/draw.png[1,1]x22")  
+			w << label("##{$here}/draw.png[2,1]x22")  
+		} } 
+		@win.sleeping(100,"Verify images")
+		w.each { |x| x.should be_a_kind_of(Gtk::Image) }
+		w.each { |x| x.pixbuf.should be_a_kind_of Gdk::Pixbuf}
+		w[1..-1].each { |x| x.pixbuf.width.should eq(22)}
+		
+ end
+ it "create a entry" do
+		w=nil
+		@win.create { stack {   w=entry("CouCou") { alert("key pressed ok") } } }
+		@win.sleeping(100,"Verify entry")
+		 w.should be_a_kind_of(Gtk::Entry)
+ end
+ it "create a ientry" do
+		w=nil
+		@win.create { stack {   w=ientry(22,min: 0, max: 100,by: 1) { alert("value modify, ok") } } }
+		@win.sleeping(100,"Verify fentry")
+		 w.should be_a_kind_of(Gtk::SpinButton)
+ end
+ it "create a fentry" do
+		w=nil
+		@win.create { stack {   w=fentry(22,min: 0, max: 10,by: 0.1) { alert("value modify, ok") } } }
+		@win.sleeping(100,"Verify fentry")
+		w.should be_a_kind_of(Gtk::SpinButton)
+ end
+ it "create fields" do
+		w=nil
+		@win.create { stack {   w=fields("aa"=>0,"bb"=>1)  } }
+		@win.sleeping(100,"Verify fields")
+		w.should be_a_kind_of(Gtk::VBox)
+		w.children.size.should eq(2)
+ end
+ it "create  fields with validation" do
+		w=nil
+		@win.create { stack {   w=fields("aa"=>0,"bb"=>1) { alert("value modify, ok") } } }
+		@win.sleeping(100,"Verify fields")
+		w.should be_a_kind_of(Gtk::VBox)
+		w.children.size.should eq(4)
+ end
+ it "create a slider" do
+		w=nil
+		@win.create { stack {   w=islider(22,min: 0, max: 100,by: 1) { |v|p v} } }
+		@win.sleeping(100,"Verify islider")
+		 w.should be_a_kind_of(Gtk::HScale)
+ end
+  it "create a canvas" do
+		w=nil
+		@win.create { stack {   w=canvas(100,100) } }
+		 w.should be_a_kind_of(Gtk::DrawingArea)
+ end
+
 end
