0e0144ee927318da63c9f8243e87af81c783adc9
Regis d'Aubarede <regis.aubarede@gmail.com>
2012-12-12 21:44:55 +0100

    footer

--- samples/make_doc.rb
+++ samples/make_doc.rb
@@ -2,15 +2,15 @@
 # Creative Commons BY-SA :  Regis d'Aubarede <regis.aubarede@gmail.com>
 # LGPL
 
-### make_doc : etract api from soure DSL code file and
-### do documentation in html form
+### make_doc : extract api from soure DSL code file and
+### make html documentation
 
 html=<<EEND
 <?DOCTYPE html>
 <html>
 <head>
-	<title>Ruiby DSL doc</title>
-	<style>
+<title>Ruiby DSL doc</title>
+<style>
 body { margin: 0px;}
 .title  {font: 35px arial,sans-serif; background: black ; color: white; text-align: center;padding: 30px;}
 .atitle {font: 25px arial,sans-serif; background: #A0A0A0; color: white;padding: 10px;}
@@ -19,13 +19,20 @@ body { margin: 0px;}
 .a { width: 100px;display: inline;}
 	</style>
 </head>
+
 <body>
+
 <div class='title'>Ruiby DSL Documentation</div>
+Ruiby Version: %s<br>
+Generated at : %s<br>
+<hr>
 <br>
-%s
+%s <!-- table des matieres -->
 <br>
 <br>
-%s
+%s  <!-- documentation -->
+<hr>
+<center>made by samples/make_doc.rb</center>
 </body>
 </html>
 EEND
@@ -53,17 +60,27 @@ def extract_doc_dsl()
 			h[name]=[name,name,api,descr]
 		elsif a=/^\s*#\s*(.*)/.match(line)
 			comment+=a[1].gsub("#","")+"<br>"
+		elsif a=/^def[\s\t]+(_.*)/.match(line)
+			comment=""
 		end
 		h
 	}
 	hdoc
 end
 
+########################## M A I N ###############################
+
 hdoc=extract_doc_dsl()
 table=hdoc.keys.sort.select {|a| (a !~ /\./) }.map { |name| htable % [name,name]}.join(" ")
 apis=hdoc.keys.sort.select {|a| (a !~ /\./) }.map {  |k|  hitem % hdoc[k] }.join("\n")
-content=html % [table,apis]
 
-output="../doc.html"
+content=html % [
+	File.read("#{File.dirname(__FILE__)}/../VERSION"),
+	Time.now.to_s,
+	table,
+	apis
+]
+
+output="#{File.dirname(__FILE__)}/../doc.html"
 File.open(output,"wb") { |f| f.write( content ) }
 system("start",Dir.pwd+"/"+output)
