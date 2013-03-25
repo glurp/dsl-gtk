d77979e7f73297e9da7d744989b682cede8ee366
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-03-25 16:30:40 +0100

    creation

--- samples/dependencies.rb
+++ samples/dependencies.rb
@@ -1,35 +1,45 @@
 # Creative Commons BY-SA :  Regis d'Aubarede <regis.aubarede@gmail.com>
 # LGPL
 # Show dependencies of current Ruiby config
-require 'tsort'
-require 'Ruiby'
-require 'gtksourceview2'
 
-class Gims
+require 'tsort'
+
+def  prequire(n)
+  puts "require #{n} ..."
+  begin
+    require  n.to_s
+  rescue Exception => e
+    puts "   Error : #{e}"
+  end
+end
+prequire ARGV.shift while ARGV.size>0
+
+class Gims 
   include TSort
-   
-  Gim = Struct.new(:name, :dependencies)
   def initialize()                   @gims = Hash.new{|h,k| h[k] = []}  end   
   def add(name, dependencies)        @gims[name] = dependencies         end
    
   def tsort_each_node(&block)        @gims.each_key(&block)             end   
   def tsort_each_child(node, &block) @gims[node].each(&block) if @gims.has_key?(node) end
+  alias :tsort_old :tsort
+  def tsort()
+     tsort_old()
+   rescue TSort::Cyclic => e
+      puts " Cycles : #{e}"
+     @gims
+   else
+     @gims
+  end
 end
 
-class String ; def lcenter(size) "%-#{size}s" % [self] end  ; end
-
-def gem_loaded()
-    lg=Gem::Specification.all.select { |s| s.activated? }
-    gims=Gims.new
+def echo_gem_loaded()
     h={}
-    lg.each { |g|  h[g.name]=g  ; gims.add(g.name, g.dependencies.map {|d| d.name}) }
-    lgs=gims.tsort.inject([]) { |a,n| a<< h[n] if h[n]; a }
-    lgs.map  { |s|  [s.name,s.version,s.dependencies.map {|d|  "#{d.name}:#{d.requirement}" }]  }
+    gims=Gem.loaded_specs.map {|n,g| h[n]=g;g}.inject(Gims.new){ |gims,gem|
+            gims.add( gem.name,gem.dependencies.map {|dep| dep.name} )
+            gims
+          }.tsort.map {|k,_| h[k]}.compact.
+          map  { |s|  [s.name,s.version,s.dependencies.map {|d|  "#{d.name}#{d.requirement}" }]  }
 end
 
 
-puts gem_loaded.map { |(a,b,c)| "#{a.lcenter(20)} ==> #{c.join(' / ')}"}  
-puts
-puts gem_loaded.map { |(a,b,c)| "gem uninstall #{a.lcenter(20)}" }.join("\n")
-puts
-puts gem_loaded.map { |(a,b,c)| "gem install #{a.lcenter(15)} -v #{b}"}.join("\n") 
+puts echo_gem_loaded.map { |(a,b,c)| "#{a.ljust(20)} #{b.to_s.ljust(7)} => #{c.join('/')}"}  
