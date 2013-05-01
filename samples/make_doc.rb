4d0b91c44bba9c6b7eaa088727f47c1977c94b2e
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-05-01 02:35:07 +0200

    cross reference hypertext and look

--- samples/make_doc.rb
+++ samples/make_doc.rb
@@ -13,10 +13,10 @@ html=<<EEND
 <style>
 body { margin: 0px;}
 .title  {font: 35px arial,sans-serif; background: black ; color: white; text-align: center;padding: 30px;}
-.atitle {font: 25px arial,sans-serif; background: #A0A0A0; color: white;padding: 10px;}
-.api    {font: 18px courier; background: #F0F0A0; color: black;}
+.atitle {font: 25px arial,sans-serif; background: #A0A0A0; color: white;padding: 10px;border-radius: 10px;margin-left:30px;}
+.api    {font: 18px courier; background: #F0F0A0; color: black;margin-left:30px;}
 .descr  {font: 15px arial,sans-serif; background: #F0F0F0; color: black;margin: 10px 0 30px 30px}
-.a { width: 100px;display: inline;}
+.a {float: left;	width: 150px;}
 	</style>
 </head>
 
@@ -28,6 +28,7 @@ Generated at : %s<br>
 <hr>
 <br>
 %s <!-- table des matieres -->
+<div style='clear: both;'> </div>
 <br>
 <br>
 %s  <!-- documentation -->
@@ -37,7 +38,7 @@ Generated at : %s<br>
 </html>
 EEND
 
-htable='<div class="a"><a href="#%s">%s</a></div>'
+htable='<span class="a"><a href="#%s">%s</a></span>'
 
 hitem=<<EEND
 <div class='atitle'><a name='%s'>%s</a></div>
@@ -52,22 +53,31 @@ def extract_doc_dsl()
 	comment=""
 	hdoc=content.split(/\r?\n\s*/).inject({}) {|h,line|
 		ret=nil
-		if a=/^def[\s\t]+([^_].*)/.match(line)
+		if a=/^def\s+([^_].*)/.match(line)
 			name=a[1].split('(')[0]
 			api=a[1].split(')')[0]+")"
 			descr=comment.gsub('#\s*',"")
 			comment=""
 			h[name]=[name,name,api,descr]
 		elsif a=/^\s*#\s*(.*)/.match(line)
-			comment+=a[1].gsub("#","")+"<br>"
-		elsif a=/^def[\s\t]+(_.*)/.match(line)
+			comment+=a[1].gsub("#","").gsub(/^\s*\*/,"<li>")+"<br>"
+		elsif /^\s*end\b/.match(line) || /^\s*def\s+(_.*)/.match(line) || /^\s*$/.match(line) || line.size==0
 			comment=""
 		end
 		h
 	}
-	hdoc
+  # make hyperlink cross reference to word definition in each description text
+	hdoc.keys.sort.each {|k|
+    name1,name2,api,descr= hdoc[k]
+    d=descr.gsub(/\w+/) { |word| (word!=k && hdoc[word]) ? make_anchor(word) : word}
+    hdoc[k]=[name1,name2,api,d] if d !=descr
+  }
+  hdoc
 end
-
+def make_anchor(word)
+   ret="<a href='##{word}'>#{word}</a>"
+   ret
+end
 ########################## M A I N ###############################
 
 hdoc=extract_doc_dsl()
