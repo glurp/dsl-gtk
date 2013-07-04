baa6bdb06cbd06b3ca6db1af3b4ae6f4d74b1388
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-07-04 19:15:17 +0200

    R

--- samples/test_include.rb
+++ samples/test_include.rb
@@ -3,7 +3,7 @@
 # Creative Commons BY-SA :  Regis d'Aubarede <regis.aubarede@gmail.com>
 # LGPL
 
-require_relative '../lib/ruiby'
+require_relative '../lib/Ruiby'
 
 class Win < Gtk::Window
 	include Ruiby
