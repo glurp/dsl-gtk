d9ef9cfb9867303824d9a1fccfedccd616b3367c
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-07-11 19:32:10 +0200

    paned by bloc

--- samples/paned.rb
+++ samples/paned.rb
@@ -3,7 +3,7 @@
 # LGPL
 
 #####################################################################
-#  panned.rb : IDE ruiby :) test paned h/v
+#  paned.rb :  test paned h/v
 #####################################################################
 # encoding: utf-8
 require_relative '../lib/Ruiby'
@@ -11,47 +11,21 @@ require_relative '../lib/Ruiby'
 
 class RubyApp < Ruiby_gtk
     def initialize
-        super("Testing Ruiby paned & threading",900,0)
-		load(__FILE__)
+        super("Testing Ruiby paned",900,800)
     end
 	def component()
-		stack do
-			sloti(htoolbar(
-				"open/ouvrir fichier"=> proc {load(ask_file_to_read(".","*.rb"))},
-				"Save/sauvegarder le fichier"=> proc {content=@edit.buffer.text
-					File.open(@file,"wb") { |f| f.write(content) } if @file && content && content.size>2
-				}
-			)) 
-			stack_paned(600,0.7) {
-				[stack_paned(900,0.2) do 
-					[stack {
-						sloti(label("Project"))
-						separator
-						vbox_scrolled(100,500) { 22.times  { |i| slot(button("eee #{i}")) } }
-					},
-					stack {
-						sloti(label("Edit"))
-						@edit=slot(source_editor(:lang=> "ruby", :font=> "Courier new 12")).editor
-					}]
-				end,
-				notebook do 
-					page("Task") { frame() { slot(button("ee")) } }
-					page("Error") { frame() { slot(button("fffffffffffffffffffffff")) } }
-					page("Console") { frame() { slot(button("fffffffffffffffffffffff")) } }
-				end
-				]
-			}
-		end
-	end
-	def change(*t)
-		puts "changer #{t.inspect}"
-	end    
-	def load(file)
-		return unless file
-		return unless File.exists?(file)
-		@file=file
-		@mtime=File.mtime(@file)
-		@edit.buffer.text=File.read(@file)
+			flow_paned(800,0.5) do
+        stack_paned(800,0.6) do
+          stack_paned(400,0.5) do
+            frame { label("Label1 ",:size=> [0,0]) }
+            frame { label("Label2 ",:size=> [0,0]) } 
+          end
+          stack {
+            frame { label("Label3 ",:size=> [200,200]) }
+          }
+        end
+        frame { label("second flow") }
+      end
 	end
 end
 
