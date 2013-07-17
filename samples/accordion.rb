048ae73ca691541fc050aeda483a50a3b9ca791d
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-07-17 19:24:42 +0200

    indentations

--- samples/accordion.rb
+++ samples/accordion.rb
@@ -9,51 +9,51 @@ Thread.abort_on_exception=true
 
 class RubyAppV < Ruiby_gtk
     def initialize() 
-		super("Testing Ruiby accordion",300,400) 
-		rposition(90,100)
-	end	
+    super("Testing Ruiby accordion",300,400) 
+    rposition(90,100)
+  end	
 
-	def component()        
-		flow {
-			accordion {
-				aitem("entry 1") {
-						alabel("xeee1") { alert("x1") }
-						alabel("xeee2") { alert("x2") }
-						alabel("xeee3") { alert("x3") }
-				}
-				aitem("entry 2") {
-						alabel "aeee1"
-						alabel "aeee2"
-						alabel "aeee3"
-				}
-				aitem("entry 3") {
-						alabel "beee1"
-						alabel "beee2"
-						alabel("beee3") { alert("bee3") }
-				}
-			}
-			stack {
-				label "x"
-				stacki {haccordion {
-				aitem("entry 1") {
-						alabel("xeee1") { alert("x1") }
-						alabel("xeee2") { alert("x2") }
-						alabel("xeee3") { alert("x3") }
-				}
-				aitem("entry 2") {
-						alabel "aeee1"
-						alabel "aeee2"
-						alabel "aeee3"
-				}
-				aitem("entry 3") {
-						alabel "beee1"
-						alabel "beee2"
-						alabel("beee3") { alert("bee3") }
-				}
-			}}
-			}				
-		}
-	end 
+  def component()        
+    flow {
+      accordion {
+        aitem("entry 1") {
+            alabel("xeee1") { alert("x1") }
+            alabel("xeee2") { alert("x2") }
+            alabel("xeee3") { alert("x3") }
+        }
+        aitem("entry 2") {
+            alabel "aeee1"
+            alabel "aeee2"
+            alabel "aeee3"
+        }
+        aitem("entry 3") {
+            alabel "beee1"
+            alabel "beee2"
+            alabel("beee3") { alert("bee3") }
+        }
+      }
+      stack {
+        label "x"
+        stacki {haccordion {
+          aitem("entry 1") {
+              alabel("xeee1") { alert("x1") }
+              alabel("xeee2") { alert("x2") }
+              alabel("xeee3") { alert("x3") }
+          }
+          aitem("entry 2") {
+              alabel "aeee1"
+              alabel "aeee2"
+              alabel "aeee3"
+          }
+          aitem("entry 3") {
+              alabel "beee1"
+              alabel "beee2"
+              alabel("beee3") { alert("bee3") }
+          }
+        }}
+      }
+    }
+  end # end component()
 end
 
 Ruiby.start do
