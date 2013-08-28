fd6670545822dc45978968bf18309902d08ae20c
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-08-28 12:52:08 +0200

    correction take-a-snapshot

--- samples/test_include.rb
+++ samples/test_include.rb
@@ -12,14 +12,15 @@ class Win < Gtk::Window
 		add(@vb=VBox.new(false, 2)) 
 		show_all
 		add_a_ruiby_button()
-    end	
+    signal_connect "destroy" do  Gtk.main_quit ; end
+	end
 	def add_a_ruiby_button() 
 		ruiby_component do
 			append_to(@vb) do 
-				slot(button("Hello Word #{@vb.children.size}") {
+				button("Hello Word #{@vb.children.size}") {
 					add_a_ruiby_button() 
-				})
-				if ARGV.any? { |a| =~/take-a-snapshot/} 
+				}
+				if ARGV.any? { |a| a=~/take-a-snapshot/} 
 					after(1000) { snapshot("snapshot_test_include.rb.png") ; exit!(0) }
 				end
 			end
@@ -27,7 +28,5 @@ class Win < Gtk::Window
 	end
 end
 
-Message.alert("CouCou")
-Gtk.init
-Win.new("application title",350,10)
-Gtk.main
+
+Ruiby.start do	Win.new("application title",350,10) end
