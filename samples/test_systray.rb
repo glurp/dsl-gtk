f5b1e237446f3dbd4d915c287a38bf0efdaff705
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-09-09 21:43:01 +0200

    nop

--- samples/test_systray.rb
+++ samples/test_systray.rb
@@ -13,24 +13,24 @@ $statusIcon=nil
 $win=nil 
 $p={}
 class Application < Ruiby_gtk
-	def update()
-		clear_append_to(@st) do 
-			label "Time is.."
-			label Time.now.to_s
-		end
-		after(3000) do
-			hide
-		end
-	end
-	  def initialize()
-		  super("test systray",100,0)
-		  default_height=0
-		  height=0
-		  
-		  threader(20)
-		  move(100,100)
-		  chrome(true)
-		  update()
+  def update()
+    clear_append_to(@st) do 
+      label "Time is.."
+      label Time.now.to_s
+    end
+    after(3000) do
+      hide
+    end
+  end
+    def initialize()
+      super("test systray",100,0)
+      default_height=0
+      height=0
+      
+      threader(20)
+      move(100,100)
+      chrome(true)
+      update()
       # component()
       systray(1000,850) do
         syst_icon  HAPPY_ICON
@@ -38,11 +38,11 @@ class Application < Ruiby_gtk
         syst_add_button "Execute Test"  do |state|  move(100,100);show; update() end
         syst_quit_button true
       end # end component()
-	  end
-	  def selection() update()  end
-	  def component()
-		clickable(:selection) { @st= stack { slot(label("...")) } }
-	  end
-	end
-	Ruiby.start { Application.new }
+    end
+    def selection() update()  end
+    def component()
+    clickable(:selection) { @st= stack { slot(label("...")) } }
+    end
+  end
+  Ruiby.start { Application.new }
 end # unless defined?
