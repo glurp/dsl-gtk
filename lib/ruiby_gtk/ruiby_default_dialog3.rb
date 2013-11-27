751e7907dc3b3cfe2ea664d31df7ab409afdb317
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-11-27 01:27:37 +0100

    indentations

--- lib/ruiby_gtk/ruiby_default_dialog3.rb
+++ lib/ruiby_gtk/ruiby_default_dialog3.rb
@@ -6,50 +6,50 @@ module Ruiby_default_dialog
   def _gooooooooooo 
   end
   
-	include ::Gtk
+  include ::Gtk
 
-	# alert(txt): modal popup with text (as in html)
-	def alert(*txt) message(:info,*txt) end
-	# modal popup with text and/or ruby Exception.
-	def error(*txt) 
-		lt=txt.map { |o| 
-			if Exception===o 
-				o.to_s + " : \n  "+o.backtrace.join("\n  ")
-			else
-				o.to_s
-			end
-		}
-		message(:error,*lt) 
-	end
-	# show a modal dialog, asking question, active bloc closure with text response
+  # alert(txt): modal popup with text (as in html)
+  def alert(*txt) message(:info,*txt) end
+  # modal popup with text and/or ruby Exception.
+  def error(*txt) 
+    lt=txt.map { |o| 
+      if Exception===o 
+        o.to_s + " : \n  "+o.backtrace.join("\n  ")
+      else
+        o.to_s
+      end
+    }
+    message(:error,*lt) 
+  end
+  # show a modal dialog, asking question, active bloc closure with text response
   # in parameters
   # prompt("Age ?") { |n| alert("Your age is #{n-1}, bravo !")
-	def prompt(txt,value="") 
-		 dialog = Dialog.new(
+  def prompt(txt,value="") 
+     dialog = Dialog.new(
       title: "Message",
-			parent: self,
-			flags: [Dialog::DESTROY_WITH_PARENT],
-			buttons: [ [Stock::OK,1], [:annulation,2] ]
+      parent: self,
+      flags: [Dialog::DESTROY_WITH_PARENT],
+      buttons: [ [Stock::OK,1], [:annulation,2] ]
     )
 
-		label=Label.new(txt)
-		entry=Entry.new().tap {|e| e.set_text(value) }
-		dialog.vbox.add(label)
-		dialog.vbox.add(entry)
-		dialog.set_window_position(:center)
+    label=Label.new(txt)
+    entry=Entry.new().tap {|e| e.set_text(value) }
+    dialog.vbox.add(label)
+    dialog.vbox.add(entry)
+    dialog.set_window_position(:center)
 
-		dialog.signal_connect('response') do |w,e|
-			rep=true
-			rep=yield(entry.text) if block_given?
-			dialog.destroy if rep
-		end
-		dialog.show_all	
-	end
+    dialog.signal_connect('response') do |w,e|
+      rep=true
+      rep=yield(entry.text) if block_given?
+      dialog.destroy if rep
+    end
+    dialog.show_all	
+  end
 
 
-	# show a modal dialog, asking yes/no question, return boolean response
-	def ask(*txt) 
-		text=txt.join(" ")
+  # show a modal dialog, asking yes/no question, return boolean response
+  def ask(*txt) 
+    text=txt.join(" ")
     md = MessageDialog.new(
       :parent => self, 
       :flags => :destroy_with_parent, 
@@ -57,82 +57,82 @@ module Ruiby_default_dialog
       :buttons_type => :yes_no,
       :message => text
     )
-		md.set_window_position(:center)
-		rep=md.run
-		md.destroy
-		return( rep==-8 )
-	end
-	
-	# travce() : like alert(), but with a  warning icone
-	def trace(*txt) message(:warning,*txt) end
+    md.set_window_position(:center)
+    rep=md.run
+    md.destroy
+    return( rep==-8 )
+  end
+  
+  # travce() : like alert(), but with a  warning icone
+  def trace(*txt) message(:warning,*txt) end
 
-	def message(style,*txt)
-		text=txt.join(" ")
+  def message(style,*txt)
+    text=txt.join(" ")
     md = MessageDialog.new(
         parent: self,
         flags: :destroy_with_parent,
         type: style,
         buttons_type: :close, 
         message: text)
-		md.set_window_position(:center)
+    md.set_window_position(:center)
     md.run
     md.destroy
-	end
+  end
   
-	# modal dialog asking a color
-	def ask_color()
-		cdia = ColorSelectionDialog.new(title: "Select color")
-		cdia.set_window_position(:center)
-		response=cdia.run
-		color=nil
+  # modal dialog asking a color
+  def ask_color()
+    cdia = ColorSelectionDialog.new(title: "Select color")
+    cdia.set_window_position(:center)
+    response=cdia.run
+    color=nil
     if response == Gtk::ResponseType::OK
         colorsel = cdia.color_selection
         color = colorsel.current_color
     end 		
-		cdia.destroy
+    cdia.destroy
     p color
-		color
-	end
+    color
+  end
 
-	########## File Edit
-	
-	# dialog showing code editor
-	def edit(filename)
-		Editor.new(self,filename)
-	end
-	
-	########## File dialog
+  ########## File Edit
+  
+  # dialog showing code editor
+  def edit(filename)
+    Editor.new(self,filename)
+  end
+  
+  ########## File dialog
 
   # ask a existent file name 
-	def ask_file_to_read(dir,filter)
-		dialog_chooser("Choose File (#{filter}) ...", Ruiby.gtk_version(3) ? :open : Gtk::FileChooser::ACTION_OPEN, Gtk::Stock::OPEN)
-	end
+  def ask_file_to_read(dir,filter)
+    dialog_chooser("Choose File (#{filter}) ...", Ruiby.gtk_version(3) ? :open : Gtk::FileChooser::ACTION_OPEN, Gtk::Stock::OPEN)
+  end
   
   # ask a filename for creation/modification
-	def ask_file_to_write(dir,filter)
-	 dialog_chooser("Save File (#{filter}) ...", Ruiby.gtk_version(3) ? :save : Gtk::FileChooser::ACTION_SAVE, Gtk::Stock::SAVE)
-	end
+  def ask_file_to_write(dir,filter)
+   dialog_chooser("Save File (#{filter}) ...", Ruiby.gtk_version(3) ? :save : Gtk::FileChooser::ACTION_SAVE, Gtk::Stock::SAVE)
+  end
   
   # ask a existent dir name
-	def ask_dir_to_read(initial_dir=nil)
-		dialog_chooser(
-				"Select existing Folder ...",
-				Gtk::FileChooser::ACTION_SELECT_FOLDER,
-				Gtk::Stock::OPEN) {|d| 
-			d.filename=initial_dir if initial_dir && File.exists?(initial_dir)
-		}
-	end
+  def ask_dir_to_read(initial_dir=nil)
+    dialog_chooser(
+        "Select existing Folder ...",
+        Gtk::FileChooser::ACTION_SELECT_FOLDER,
+        Gtk::Stock::OPEN) {|d| 
+      d.filename=initial_dir if initial_dir && File.exists?(initial_dir)
+    }
+  end
   
   # ask  a dir name 
-	def ask_dir_to_write(initial_dir=nil)
-		dialog_chooser(
-			"Select Folder or create one ...", 
-			Gtk::FileChooser::ACTION_SELECT_FOLDER ,
-			Gtk::Stock::OPEN) {|d|
-			d.filename=initial_dir if initial_dir 
-		}
-	end
-	def dialog_chooser(title, action, button)
+  def ask_dir_to_write(initial_dir=nil)
+    dialog_chooser(
+      "Select Folder or create one ...", 
+      Gtk::FileChooser::ACTION_SELECT_FOLDER ,
+      Gtk::Stock::OPEN) {|d|
+      d.filename=initial_dir if initial_dir 
+    }
+  end
+  def dialog_chooser(title, action, button)
       if Ruiby.gtk_version(3)
         dialog = Gtk::FileChooserDialog.new(
             :title => title, 
@@ -152,22 +152,22 @@ module Ruiby_default_dialog
           [button, Gtk::Dialog::RESPONSE_ACCEPT]
         )
     end
-		dialog.set_window_position(:center)
-		yield(dialog) if block_given?
-	    ret = ( dialog.run == Gtk::Dialog::RESPONSE_ACCEPT ? dialog.filename : nil )rescue false
-	    dialog.destroy
-	    ret ? ret.gsub('\\','/') : ""
-	end
+    dialog.set_window_position(:center)
+    yield(dialog) if block_given?
+      ret = ( dialog.run == Gtk::Dialog::RESPONSE_ACCEPT ? dialog.filename : nil )rescue false
+      dialog.destroy
+      ret ? ret.gsub('\\','/') : ""
+  end
 end
 
 #  common dialog to be use for direct  call in none Ruiby context classe :
 #  Message.alert("ddde",'eee')
 class Message
-	class Embbeded  < ::Gtk::Window
-		include ::Ruiby_default_dialog
-	end
-	def self.alert(*txt) Embbeded.new.alert(*txt) end
-	def self.error(*txt) Embbeded.new.error(*txt) end
-	def self.ask(*txt)   Embbeded.new.ask(*txt)   end
-	def self.prompt(txt,value="")  Embbeded.new.alert(*txt) end
+  class Embbeded  < ::Gtk::Window
+    include ::Ruiby_default_dialog
+  end
+  def self.alert(*txt) Embbeded.new.alert(*txt) end
+  def self.error(*txt) Embbeded.new.error(*txt) end
+  def self.ask(*txt)   Embbeded.new.ask(*txt)   end
+  def self.prompt(txt,value="")  Embbeded.new.alert(*txt) end
 end
