bc7e4cc70b4a79a2778659809ad2fe6665e8d479
Regis d'Aubarede <regis.aubarede@gmail.com>
2013-12-09 20:12:33 +0100

    indentations

--- samples/color.rb
+++ samples/color.rb
@@ -11,13 +11,13 @@ require_relative '../lib/Ruiby'
 
 Ruiby.app title: "Color test for button/label" do
     a=nil
-		stack {
+    stack {
       w=button("test for "+ (ARGV.size>0 ? "Gtk3" : "Gtk2"),{font:'Tahoma bold italic 34'}) { exit! }
       widget_properties
-			flow { a=buttoni("(1)green+blue/red    ",{fg:"#007777",bg:"#FF0000"}) ;button("  default  ",{}) ;button("  default  ",{}) ;}
-			flow { buttoni("red / green bold",{bg:"#00FF00",fg:"#FF0000",font:"Arial bold 32"}) ;button("red / blue",{}) ;button("red / blue",{}) ;}
-			flow { label("red / blue",{bg:"#0000FF",fg:"#FF0000"}) ;label("red / blue",font:"Arial bold 32") ;label("red / blue",{}) ;}
-			flow { label("red / green / Arial",{bg:"#00FF00",fg:"#FF0000",font:"Arial 32"}) ;label("red / blue",{}) ;label("red / blue",{}) ;}
+      flow { a=buttoni("(1)green+blue/red    ",{fg:"#007777",bg:"#FF0000"}) ;button("  default  ",{}) ;button("  default  ",{}) ;}
+      flow { buttoni("red / green bold",{bg:"#00FF00",fg:"#FF0000",font:"Arial bold 32"}) ;button("red / blue",{}) ;button("red / blue",{}) ;}
+      flow { label("red / blue",{bg:"#0000FF",fg:"#FF0000"}) ;label("red / blue",font:"Arial bold 32") ;label("red / blue",{}) ;}
+      flow { label("red / green / Arial",{bg:"#00FF00",fg:"#FF0000",font:"Arial 32"}) ;label("red / blue",{}) ;label("red / blue",{}) ;}
       flow { properties("props of (1)",get_config(a)) ; properties("props of child of(1)",get_config(a.child))}
       button("dialog...") do
         dialog("title") {
