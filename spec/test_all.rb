31d4879629a7622150bf34bfbf7858a5be0f698a
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-01-15 18:44:35 +0100

    add canvas test

--- spec/test_all.rb
+++ spec/test_all.rb
@@ -1,4 +1,7 @@
+# Creative Commons BY-SA :  Regis d'Aubarede <regis.aubarede@gmail.com>
+# LGPL
 load 'test_layout.rb'
 load 'test_basics_widget.rb'
+load 'test_canvas.rb'
 load 'test_dialogs.rb'
 load 'test_composites.rb'
