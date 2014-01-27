aba66e0abb51ff9b15d73bbcdc59d036b289bac0
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-01-27 19:23:07 +0100

    unused var , indentations

--- samples/video.rb
+++ samples/video.rb
@@ -3,17 +3,17 @@ require_relative '../lib/Ruiby'
 # Creative Commons BY-SA :  Regis d'Aubarede <regis.aubarede@gmail.com>
 # LGPL
 
-    Ruiby.app width: 600,height: 400 do
-      video_file= ARGV[0] || "d:/usr/XT.avi"
-      set_title(video_file)
-      video_url="file:///#{video_file}"
-      stack do
-        @v=video(video_url,600,400-40)  {|progress| @prog && @prog.progress=progress*100 }
-        flowi {
-          buttoni("  Start  ") {  @v.play }
-          buttoni("  Stop  ") {  @v.stop }
-          @prog=slider(0,0,100.0) { |pos| @v.progress= pos/100.0}
-          buttoni("  Exit  ") { exit!(0) }
-        }
-      end
-    end
+Ruiby.app width: 600,height: 400 do
+  video_file= ARGV[0] || "d:/usr/XT.avi"
+  set_title(video_file)
+  video_url="file:///#{video_file}"
+  stack do
+    @v=video(video_url,600,400-40)  {|progress| @prog && @prog.progress=progress*100 }
+    flowi {
+      buttoni("  Start  ") {  @v.play }
+      buttoni("  Stop  ") {  @v.stop }
+      @prog=slider(0,0,100.0) { |pos| @v.progress= pos/100.0}
+      buttoni("  Exit  ") { exit!(0) }
+    }
+  end
+end
