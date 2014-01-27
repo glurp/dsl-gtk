5445fb50ccd94396ada0e5fc0b6fa4fad1957d38
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-01-27 19:22:51 +0100

    unused var , indentations

--- samples/test_systray.rb
+++ samples/test_systray.rb
@@ -9,6 +9,7 @@ HAPPY_ICON   ="media/face_smile_big.png"
 Thread.abort_on_exception=true
 
 unless defined?($statusIcon)
+  
 $statusIcon=nil
 $win=nil 
 $p={}
@@ -22,27 +23,28 @@ class Application < Ruiby_gtk
       hide
     end
   end
-    def initialize()
-      super("test systray",100,0)
-      default_height=0
-      height=0
-      
-      threader(20)
-      move(100,100)
-      chrome(true)
-      update()
-      # component()
-      systray(1000,850, icon: "media/angel.png") do
-        syst_icon  HAPPY_ICON
-        syst_add_button "Reload"        do |state| load(__FILE__) rescue log $! ; end
-        syst_add_button "Execute Test"  do |state|  move(100,100);show; update() end
-        syst_quit_button true
-      end # end component()
-    end
-    def selection() update()  end
-    def component()
-    clickable(:selection) { @st= stack { slot(label("...")) } }
-    end
+  def initialize()
+    super("test systray",100,0)
+    default_height=0
+    height=0
+    
+    threader(20)
+    move(100,100)
+    chrome(true)
+    update()
+    # component()
+    systray(1000,850, icon: "media/angel.png") do
+      syst_icon  HAPPY_ICON
+      syst_add_button "Reload"        do |state| load(__FILE__) rescue log $! ; end
+      syst_add_button "Execute Test"  do |state|  move(100,100);show; update() end
+      syst_quit_button true
+    end # end component()
   end
-  Ruiby.start { Application.new }
+  def selection() update()  end
+  def component()
+    clickable(:selection) { @st= stack { slot(label("...")) } }
+  end
+end
+Ruiby.start { Application.new }
+
 end # unless defined?
