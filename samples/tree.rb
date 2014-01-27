0c6bb67f20a6ec9e1cf9bc167dea0aae0c562162
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-01-27 19:23:04 +0100

    unused var , indentations

--- samples/tree.rb
+++ samples/tree.rb
@@ -7,34 +7,34 @@ require_relative '../lib/Ruiby'
 
 class RubyApp < Ruiby_gtk
     def initialize() 
-		super("Testing Ruiby Table / cell span",300,400) 
-		threader(10)
-	end	
-	def component()        
-		stack {
-			@tg=tree_grid(%w{name 0age prename ok #image})
-			buttoni("dailog...") {
-				rep=dialog("modal window...") {
-					label("eee")  
-					list("aa",100,100)
-				}
-				alert("Response was "+rep.to_s)
-			}
-			buttoni("dailog async...") {
-				dialog_async("modal window...",{response: proc {|a| alert(a);true}}) {
-					label("eee") 
-					list("aa",100,100)
-				}
-			}
+	super("Testing Ruiby Table / cell span",300,400) 
+	threader(10)
+    end
+    def component()        
+	stack {
+	@tg=tree_grid(%w{name 0age prename ok #image})
+	    buttoni("dailog...") {
+		rep=dialog("modal window...") {
+		    label("eee")  
+		    list("aa",100,100)
 		}
-		@tg.set_data({
-			b1: {
-				a1: [11,"eee",true,"media/face_crying.png"],
-				a2: [33,"Dee",false,"media/draw.png"],
-				#a3: [33,"Dee",false,"open"],
-			},
-		})
-	end  
+		    alert("Response was "+rep.to_s)
+	    }
+	    buttoni("dailog async...") {
+		dialog_async("modal window...",{response: proc {|a| alert(a);true}}) {
+		    label("eee") 
+		    list("aa",100,100)
+		}
+	    }
+	}
+	@tg.set_data({
+	    b1: {
+		a1: [11,"eee",true,"media/face_crying.png"],
+		a2: [33,"Dee",false,"media/draw.png"],
+		#a3: [33,"Dee",false,"open"],
+	    },
+	})
+    end  
 end
 
-Ruiby.start { window = RubyApp.new }
+Ruiby.start { RubyApp.new }
