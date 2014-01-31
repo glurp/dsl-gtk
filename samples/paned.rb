fffd95bb6a903dbd9a30117323ff711e439b3a75
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-02-01 00:43:23 +0100

    to 1.0

--- samples/paned.rb
+++ samples/paned.rb
@@ -14,19 +14,27 @@ class RubyApp < Ruiby_gtk
         super("Testing Ruiby paned",900,800)
     end
 	def component()
-			flow_paned(800,0.5) do
-        stack_paned(800,0.6) do
-          stack_paned(400,0.5) do
-            frame { 
-              label("Label1 ",:size=> [100,100]) 
+      stack do
+         menu_bar {
+           menu("File Example") {
+               menu_button("Open") { alert("o") }
+               menu_button("Close") { alert("i") }
             }
-            frame { label("Label2 ",:size=> [0,0]) } 
+        }
+        flow_paned(800,0.5) do
+          stack_paned(800,0.6) do
+            stack_paned(400,0.5) do
+              frame { 
+                label("Label1 ",:size=> [100,100]) 
+              }
+              frame { label("Label2 ",:size=> [0,0]) } 
+            end
+            stack {
+              frame { label("Label3 ",:size=> [200,200]) }
+            }
           end
-          stack {
-            frame { label("Label3 ",:size=> [200,200]) }
-          }
+          frame { label("second flow", bg: "#FF0000") }
         end
-        frame { label("second flow") ; style(bg: "#FF0000")}
       end
 	end
 end
