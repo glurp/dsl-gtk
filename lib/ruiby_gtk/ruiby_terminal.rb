f22375af163eba3ce30cd34279586318b9ce0e44
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-09-05 13:34:42 +0200

    execute systems commands on terminal

--- lib/ruiby_gtk/ruiby_terminal.rb
+++ lib/ruiby_gtk/ruiby_terminal.rb
@@ -1,9 +1,12 @@
 # Creative Commons BY-SA :  Regis d'Aubarede <regis.aubarede@gmail.com>
 # LGPL
 #
+require 'open3'
+
 class Terminal
   def initialize(tv,win)
     @tv=tv
+    #@tv.override_font(  Pango::FontDescription.new("courier bold 10")) 
     @tb=tv.buffer
     @win=win
     def tv.terminal(term=nil) @term ? @term : (@term=term) end
@@ -15,7 +18,6 @@ class Terminal
   end
   def append_and_prompt(text)
     text=text.to_s
-    puts "append_and_prompt #{text.to_s[0..50]}... // #{text.size}"
     buf_start, end_iter = @tb.bounds
     @tb.insert(end_iter,"\n#{text}\n> ")
     insert=@tb.create_mark("insert", @tb.bounds.last, true)
@@ -29,11 +31,17 @@ class Terminal
     buf_start, end_iter = @tb.bounds
     @tb.insert(end_iter,text)
     insert=@tb.create_mark("insert", @tb.bounds.last, true)
+    @win.after(1) {
+      vscroll=@tv.parent.vadjustment
+      vscroll.value = vscroll.upper+8
+    }
   end
   def replace_current(text)
-    #delete current line
-    #@tb.bounds.last
-    append_and_prompt(text)
+    l = @tb.line_count
+    start = @tb.get_iter_at(line: l-1)  
+    eend = @tb.end_iter()
+    @tb.delete(start,eend)    
+    append(text)
   end
   
   def get_history(n=-1)
@@ -55,7 +63,7 @@ class Terminal
     return if line.size==0
     
     @history << line if line!= @history.last
-    @position= @history.size-1
+    @position= @history.size
     cmd,*args=line.split(/\s+/)
     case cmd 
       when /^vim?$/
@@ -67,6 +75,20 @@ class Terminal
       when "tree"
         @win.wtree(@win)
         append_and_prompt ""
+      when "help"
+        append_and_prompt <<-EEND
+          help                       : show this message
+          vim filename               : edit file
+          clear (alias clr or cls)   : clear console
+          tree                       : print widget tree of main window in log
+          h [number]                 : show history commands / recal command
+          show object                : show methods of object in log window
+          echo params...             : evaluate params in main window and print values
+          techo params...            : (debug terminal) evaluate in terminal object and print
+          os commands (ls,find,perl...)
+          exit                       : destroy this terminal
+          ; see ruiby_terminal.rb for append some commands :)
+        EEND
       when "h","history"
         if args.size==0
           s=@history.size
@@ -87,6 +109,18 @@ class Terminal
         append_and_prompt( self.instance_eval(args.join(" ")))
       when "exit"
         @tv.parent.parent.parent.parent.close
+      when /^(ls|ll|mv|grep|find|gfind|awk|ruby|sh|python|perl|julia)$/
+        if @win.ask("execute <<#{line}>>  ?")
+          append ""
+          Open3.popen3(*([cmd]+args)) do |i,o,e|
+            while q=IO.select([o]) 
+              append s=q.first.first.read
+              break if !s || s.size==0
+              Ruiby.update
+            end
+          end
+          append_and_prompt(">>> end execute.")
+        end
       else
         begin
           res=@win.instance_eval(line)
@@ -108,7 +142,7 @@ module Ruiby_default_dialog
     
     termBuffer = TextBuffer.new
     tv=TextView.new(termBuffer)
-    tv.override_font(  Pango::FontDescription.new("Courier new 10 bold")) 
+    tv.override_font(  Pango::FontDescription.new("courier bold 10")) 
     sw.add_with_viewport(tv)
     tv.signal_connect('key-press-event') do |w,ev|
       _process_terminal_key(w,ev,termBuffer)
