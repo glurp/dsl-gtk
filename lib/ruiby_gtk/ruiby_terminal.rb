a6691c06ef96cd6d42ce8b9fe66a9411bc385cc0
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-09-05 12:37:34 +0200

    terminal almost ready

--- lib/ruiby_gtk/ruiby_terminal.rb
+++ lib/ruiby_gtk/ruiby_terminal.rb
@@ -5,28 +5,35 @@ class Terminal
   def initialize(tv,win)
     @tv=tv
     @tb=tv.buffer
-	@win=win
+    @win=win
     def tv.terminal(term=nil) @term ? @term : (@term=term) end
-	tv.terminal(self)
+    tv.terminal(self)
     @@history||= ["nop"]
     @history=@@history
     @position=@history.size-1
-	append_and_prompt ""
+    append_and_prompt ""
   end
   def append_and_prompt(text)
-	puts "append_and_prompt #{text.inspect} // #{text.size}"
-	buf_start, end_iter = @tb.bounds
+    text=text.to_s
+    puts "append_and_prompt #{text.to_s[0..50]}... // #{text.size}"
+    buf_start, end_iter = @tb.bounds
     @tb.insert(end_iter,"\n#{text}\n> ")
-	buf_start, end_iter = @tb.bounds
-	eend=@tb.create_mark("eend", end_iter, false)
-	@tv.scroll_to_mark(eend,0.0,true,1.0,1.0)
+    insert=@tb.create_mark("insert", @tb.bounds.last, true)
+    @win.after(100) {
+      vscroll=@tv.parent.vadjustment
+      vscroll.value = vscroll.upper+8
+    }
+  end
+  def append(text)
+    text=text.to_s
+    buf_start, end_iter = @tb.bounds
+    @tb.insert(end_iter,text)
+    insert=@tb.create_mark("insert", @tb.bounds.last, true)
   end
   def replace_current(text)
-	puts "replace_current by #{text}"
-	buf_start, end_iter = @tb.bounds
-    @tb.insert(end_iter,"\n#{text}")
-	eend=@tb.create_mark("eend", end_iter, false)
-	@tv.scroll_to_mark(eend,0.0,true,1.0,1.0)
+    #delete current line
+    #@tb.bounds.last
+    append_and_prompt(text)
   end
   
   def get_history(n=-1)
@@ -35,33 +42,57 @@ class Terminal
   end
   def set_history(n=-1)
     text=get_history(n)
-	replace_current(text)
+    replace_current(text)
   end
   def get_line()
-	(@tb.text||"").split(/\r?\n/).last.gsub(/^\s*>\s*/,"")
+    (@tb.text||"").split(/\r?\n/).last.gsub(/^\s*>\s*/,"")
   end
+  
   def execute(line=nil)
     line||=get_line
-	puts "lastline: #{line}"
-	line=line.strip
-	return if line.size==0
-	
-	@history << line if line!= @history.last
-	@position= @history.size-1
-	cmd,*args=line.split(/\s+/)
-	case cmd 
-	  when /^vim?$/
-		system("vim",*args)
-	  when "show"
-		@win.show_methods( eval(args.first) )
-	  when "echo"
-	    append_and_prompt(eval(args.join(" ")))
-	  else
-		begin
-	      res=@win.instance_eval(line)
-		  append_and_prompt(res.to_s)
-		end
-	end
+    puts "lastline: #{line}"
+    line=line.strip
+    return if line.size==0
+    
+    @history << line if line!= @history.last
+    @position= @history.size-1
+    cmd,*args=line.split(/\s+/)
+    case cmd 
+      when /^vim?$/
+        system("vim",*args)
+        append_and_prompt ""
+      when "clear","clr","cls"
+        @tb.text=""
+        append_and_prompt ""        
+      when "tree"
+        @win.wtree(@win)
+        append_and_prompt ""
+      when "h","history"
+        if args.size==0
+          s=@history.size
+          append_and_prompt @history.each_with_index.map {|l,i| 
+             "   %3d >%s" % [s-i,l]
+            }.last(40).join("\n")
+        else
+          cmd=@history[-args.first.to_i-1]
+          append_and_prompt "<<#{cmd}>>"
+          execute cmd
+        end
+      when "show"
+        @win.show_methods( @win.instance_eval(args.first) )
+        append_and_prompt ""
+      when "echo"
+        append_and_prompt( @win.instance_eval(args.join(" ")))
+      when "techo"
+        append_and_prompt( self.instance_eval(args.join(" ")))
+      when "exit"
+        @tv.parent.parent.parent.parent.close
+      else
+        begin
+          res=@win.instance_eval(line)
+          append_and_prompt(res.to_s)
+        end
+    end
   end
 end
 
@@ -77,13 +108,14 @@ module Ruiby_default_dialog
     
     termBuffer = TextBuffer.new
     tv=TextView.new(termBuffer)
+    tv.override_font(  Pango::FontDescription.new("Courier new 10 bold")) 
     sw.add_with_viewport(tv)
     tv.signal_connect('key-press-event') do |w,ev|
       _process_terminal_key(w,ev,termBuffer)
     end
     Terminal.new(tv,self) 
     
-    wdlog.child.add(sw)
+    wdlog.child.pack_start(sw,true,true)
     wdlog.signal_connect('response') { wdlog.destroy }
     wdlog.show_all
     wdlog
@@ -95,21 +127,21 @@ module Ruiby_default_dialog
     down=65364
     enter=65293
     if ev.keyval==up
-	   w.terminal.set_history(-1)
+       w.terminal.set_history(-1)
+       return true
     end
     if ev.keyval==down
-	   w.terminal.set_history(1)
+       w.terminal.set_history(1)
+       return true
     end
     if ev.keyval==enter
-	   w.terminal.execute()
-	   return true
+       w.terminal.execute()
+       return true
     end      
-      #show_methods termBuffer
-      #after(1) { termBuffer.insert "Enter" }
     false
   rescue Exception => e
     w.terminal.append_and_prompt(e.to_s+"\n  "+e.backtrace.join("\n  "))
-	false
+    true
   end
 end
 
