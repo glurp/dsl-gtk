5921dae5c26bcf5a5546b786c848f910cdefbb64
Regis d'Aubarede <regis.aubarede@gmail.com>
2014-09-16 22:29:23 +0200

    change mrl test, rotation viewer

--- samples/video.rb
+++ samples/video.rb
@@ -6,7 +6,7 @@ require_relative '../lib/Ruiby'
 Ruiby.app width: 600,height: 400 do
   video_file= ARGV[0] || "d:/usr/XT.avi"
   set_title(video_file)
-  video_url="file:///#{video_file}"
+  video_url=File.exists?(video_file) ?  "file:///#{video_file}" : video_file 
   stack do
     @v=video(video_url,600,400-40)  {|progress| @prog && @prog.progress=progress*100 }
     flowi {
@@ -16,4 +16,23 @@ Ruiby.app width: 600,height: 400 do
       buttoni("  Exit  ") { exit!(0) }
     }
   end
+  @v.play 
+  after(10000) { 
+    @v.view.rotation_center_x=Clutter::Vertex.new(300,200,0)
+    anim(50) {
+      @v.view.rotation_angle_x+=0.1
+      @v.view.rotation_angle_y+=0.1
+    }
+  }
+  @luri=Dir.glob('d:/usr/local/video/avi/*.avi').map {|a|"file:///#{a}"}
+  anim(40000) {
+     if @luri.size>1
+       @v.stop
+       uri=@luri[rand(@luri.size-1).round]
+       #log "switch to #{uri}"
+       @v.url= uri
+       @v.progress=rand
+       @v.play     
+     end
+  }
 end
