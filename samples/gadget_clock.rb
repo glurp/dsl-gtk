591edabf10e41740a9edc9cb4b6c2e6208761998
Regis d'Aubarede <regis.aubarede@gmail.com>
2015-05-12 21:30:13 +0200

    center clock

--- samples/gadget_clock.rb
+++ samples/gadget_clock.rb
@@ -26,7 +26,7 @@ Ruiby.app width: 120,height: 120, title: "Clock" do
   end
   def draw_aig(cv,x,y,r,pos,width,color)
     a=(pos+0.75)*(Math::PI*2.0)
-	if width<2
+    if width<2
       @cv.draw_line([x,y,x+r*Math.cos(a),y+r*Math.sin(a)],color,width) 
     else
       a1=(pos+0.75-width/60.0)*(Math::PI*2.0)
@@ -45,8 +45,10 @@ Ruiby.app width: 120,height: 120, title: "Clock" do
     ssize= size()
     @cv.set_size_request(ssize.first-2,ssize.last-2)
     cx,cy=ssize.first/2,ssize.last/2
+    cy=cx if cy>cx
     ray=cx*0.8
-    @cv.draw_rectangle(0,0,cx*2,cy*2,0,$BG,$BG,0)
+    cy*=0.87
+    @cv.draw_rectangle(0,0,ssize.first,ssize.last,0,$BG,$BG,0)
     @cv.draw_circle(cx,cy,ray,"#A0A0A0","#FFF",3)
     c=[[3,1],[5,3],[8,5]]
     60.times { |i| 
@@ -63,7 +65,8 @@ Ruiby.app width: 120,height: 120, title: "Clock" do
     hour,min,sec=now.hour,now.min,now.sec
     min,sec=(now.to_i%3600)/60.0,now.to_f%60
     hour+=min/60.0
-    @cv.draw_text(cx-35,cy*2-3,"%02d:%02d:%02d" % [hour,min,sec],1.6,"#FFF")
+    
+    @cv.draw_text(cx-35,[ssize.last-4,cy*2+16].min,"%02d:%02d:%02d" % [hour,min,sec],1.6,"#FFF")
     draw_aig(cv,cx,cy,ray/2,hour/12.0,4,"#FFF")
     draw_aig(cv,cx,cy,ray*2/3,min/60.0,2,"#FFF")
     draw_aig(cv,cx,cy,ray*0.9,sec/60.0,1,"#888")
@@ -73,5 +76,6 @@ Ruiby.app width: 120,height: 120, title: "Clock" do
   
   anim(1000) { @cv.redraw  }
   after(1) { @cv.redraw  }
+  anim(3000) { set_keep_above(true) } 
 end
 
