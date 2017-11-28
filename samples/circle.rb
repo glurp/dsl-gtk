79f6f2dba6164229a88222619d1bca9a61e307fe
Regis d'Aubarede <regis.aubarede@gmail.com>
2017-11-28 19:10:41 +0100

    bg with gradient

--- samples/circle.rb
+++ samples/circle.rb
@@ -7,9 +7,10 @@ require_relative '../lib/Ruiby.rb'
 Ruiby.app width: 400, height:200, title: "Drawing Ruiby/canvas test" do
   pt=[0,0]
   stack do
+    @lab=labeli("@")
 	  canvas(0,0) do
       on_canvas_draw do |w,ctx|
-        w.draw_rectangle(10,10,180,180,30,"#F00","#00F",1)
+        w.draw_rectangle(0,0,400,200,0,nil,%w{g tb #AAA #555 - - - - - - - #AAA},2)
         w.draw_rectangle(30,30,70,70,[0,0,20,20],"#F00","#00A",1)
         w.draw_rectangle(60,110,50,50,[10,10,0,0],"#F00","#A0A",1)
         w.draw_rectangle(110,15,50,50,[0,10,10,0],"#F00","#AAA",1)
@@ -19,7 +20,8 @@ Ruiby.app width: 400, height:200, title: "Drawing Ruiby/canvas test" do
         
         w.draw_rectangle(pt.first-2,pt.last-2,3,3,0,"#000","#000",0) if pt!=[0,0]
       end
-      on_canvas_button_press {|w,e| pt=[e.x,e.y] }
+      on_canvas_button_press {|w,e| pt=[e.x,e.y] ; @lab.text=pt.to_s ; pt}
+      on_canvas_button_motion {|w,e,o| pt=[e.x,e.y] ; @lab.text=pt.to_s ; pt}
 	  end
   end
 end
